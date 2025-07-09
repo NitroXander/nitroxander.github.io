9461c4de0f44b6b1f18684231e0dd777663046c6
Kalana Jayaweera <kalana123uk@gmail.com>
2025-07-09 21:08:26 +0530

    try to run this

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,20 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ],
   },
+   server: {
+        port: 3000,
+        open: true,
+        cors: true,
+    },
+    base: './',
 })
